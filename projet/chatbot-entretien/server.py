0f3ec7d65933d2fb75627e2828169a942b21541f
sylvain regnier <regnier.sylvain@yahoo.fr>
2025-03-24 23:53:21 +0100

    modif server

--- projet/chatbot-entretien/server.py
+++ projet/chatbot-entretien/server.py
@@ -122,7 +122,7 @@ print("\n✅ Serveur prêt, lancement de l'API...")
 app = FastAPI(root_path="/chatbot")
 
 app.mount("/chatbot/static", StaticFiles(directory="static"), name="static")
-app.mount("/documents", StaticFiles(directory="documents"), name="documents")
+app.mount("/chatbot/documents", StaticFiles(directory="documents"), name="documents")
 
 
 
