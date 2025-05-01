fd11fb4eeb585f0c3429d93997c89399f2ae4a66
sylvain regnier <regnier.sylvain@yahoo.fr>
2025-05-01 10:56:41 +0200

    modification couleur

--- projet/morpion-main/assets/js/script.js
+++ projet/morpion-main/assets/js/script.js
@@ -19,7 +19,7 @@ document.querySelector('tbody').addEventListener('click', function(event) {
        
         if (joueur === 0 && cellule.innerHTML !== "X" && cellule.innerHTML !== "O") {
             cellule.innerHTML = "X";
-            cellule.style.color = "white";
+            cellule.style.color = "black";
             console.log("Humain joue :", cellule.innerHTML);
             joueur = 1; 
             verifierGagnerligne();
